d9661b2835befbc53acbb07cb63ee30e74df3246
maddog0057 <maddog0057@gmail.com>
2018-07-03 20:23:42 -0400

    switches

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ with open("config.json", 'r') as json_data_file:
 
 url = config["radarr"]["url"]
 api = config["radarr"]["token"]
+channel = config["discord"]["channel"]
 bot = commands.Bot(command_prefix='!') 
-ia = imdb.IMDb()
+ia = imdb.IMDb()    
 
-def format(mtitle, count):
+def movierequest(mtitle, count):
     query = ia.search_movie(mtitle)
     if(count < 3):
         place = str("top")
@@ -23,6 +24,7 @@ def format(mtitle, count):
     while True:
         res=query[int(count)]
         resno = int(count)+1
+        mid[count] = res.movieID
         ia.update(res, 'plot')
         plot =  str(res['plot'][0])
         plot = plot.split('.', 1)[0]
@@ -30,7 +32,10 @@ def format(mtitle, count):
         count+=1
         if(count > finalcount):
             break
-    return embed
+    return embed mid
+
+def mediaget(media, mid):
+    await 
 
 @bot.event
 async def on_ready():
@@ -44,6 +49,33 @@ async def search(channel, inmsg):
     embed = format(inmsg, 0)
     await channel.send(embed=embed)
     embed = None
+
+@bot.command()
+async def request(chx, media, search):
+    count = 0
+    mediaswitch = {
+        "movie": movierequest(search, count),
+        "tvseries": tvrequest(search, count),
+        "music": musicrequest(search, count),
+    }
+    request = mediaswitch.get(media)
+    embed = request(0)
+    mid = request(1)
+    await channel.send(embed=embed)
+    response = await channel.wait_for_message()
+    respswitch = {
+        1: mediaget(media, mid[0]),
+        2: mediaget(media, mid[1]),
+        3: mediaget(media, mid[2]),
+        'next': mediaswitch.get(media)
+    }
+    count+=3
+    request = respswitch.get(response)
+
+
+
+
+
 
 
 bot.run(config["discord"]["token"])
